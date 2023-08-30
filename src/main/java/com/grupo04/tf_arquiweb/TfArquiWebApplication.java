e76a966bf71fb35fead938d68c55266d18ba4f8f
#G4br13l <gabitoal28@gmail.com>
2023-08-30 00:36:57 -0500

    Initial commit

---
+++ src/main/java/com/grupo04/tf_arquiweb/TfArquiWebApplication.java
@@ -0,0 +1,13 @@
+package com.grupo04.tf_arquiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfArquiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfArquiWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo04/tf_arquiweb/TfArquiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo04.tf_arquiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfArquiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
