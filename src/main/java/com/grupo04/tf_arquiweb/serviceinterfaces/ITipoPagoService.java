42723f3b5cda663182d42aaaa16a0589729597a7
iShinmazinger <mazinger05x72@gmail.com>
2023-09-06 17:53:01 -0500

    TipoPagoService añadido

---
+++ src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/ITipoPagoService.java
@@ -0,0 +1,11 @@
+package com.grupo04.tf_arquiweb.serviceinterfaces;
+
+import com.grupo04.tf_arquiweb.entities.TipoPago;
+
+import java.util.List;
+
+public interface ITipoPagoService {
+    public void insert(TipoPago tipoPago);
+    public List<TipoPago> list();
+    public void delete(int TipoPagoID);
+}
