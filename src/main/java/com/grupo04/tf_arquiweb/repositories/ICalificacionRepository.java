e228c7793e0ca71e12395388ae976c42f2cfdb60
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-09-05 22:52:13 -0500

    Se agregó ICalificacionRepository

--- src/main/java/com/grupo04/tf_arquiweb/repositories/ICalificacionRepository.java
+++ src/main/java/com/grupo04/tf_arquiweb/repositories/ICalificacionRepository.java
@@ -1,2 +1,10 @@
-package com.grupo04.tf_arquiweb.repositories;public interface ICalificacionRepository {
+package com.grupo04.tf_arquiweb.repositories;
+
+import com.grupo04.tf_arquiweb.entities.Calificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICalificacionRepository extends JpaRepository<Calificacion,Integer> {
+
 }
