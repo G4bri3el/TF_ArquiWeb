bb941de048a89d5bb4713324755fb55a26777386
Mafer Medina <medina.mafer27@gmail.com>
2023-09-06 18:13:19 -0500

    Actualización Usuario Service

--- src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/IUsuarioService.java
+++ src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/IUsuarioService.java
@@ -1,11 +1,10 @@
 package com.grupo04.tf_arquiweb.serviceinterfaces;
 
 import com.grupo04.tf_arquiweb.entities.Usuario;
-import com.grupo04.tf_arquiweb.entities.Usuarios;
 
 import java.util.List;
 
-public interface IUserService { //Se pone todos los metodos que tendra la entidad
+public interface IUsuarioService { //Se pone todos los metodos que tendra la entidad
 
     public void insert(Usuario user);
 
