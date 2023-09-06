3dc3edf4f5d3806faa395a34d4bdd61d92bf6006
Mafer Medina <medina.mafer27@gmail.com>
2023-09-05 23:00:57 -0500

    ReservaServiceImplement

--- src/main/java/com/grupo04/tf_arquiweb/serviceimplements/ReservaServiceImplement.java
+++ src/main/java/com/grupo04/tf_arquiweb/serviceimplements/ReservaServiceImplement.java
@@ -1,2 +1,30 @@
-package com.grupo04.tf_arquiweb.serviceimplements;public class ReservaServiceImplement {
+package com.grupo04.tf_arquiweb.serviceimplements;
+
+
+import com.grupo04.tf_arquiweb.entities.Reserva;
+import com.grupo04.tf_arquiweb.repositories.IReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReservaServiceImplement {
+
+    @Autowired //inyeccion de dependencias
+    private IReservaRepository reR; //capa service con repo
+
+
+    public void insert(Reserva reserva){
+        reR.save(reserva);
+    }
+
+    public void delete(int ReservaID){
+        reR.deleteById(ReservaID);
+    }
+
+    public List<Reserva> list(){
+        return reR.findAll();
+    }
+
 }
