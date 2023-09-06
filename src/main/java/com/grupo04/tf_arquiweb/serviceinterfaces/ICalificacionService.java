66860294b0d7caddc36ef3ffa22237f3fd273483
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-09-05 23:10:24 -0500

    ICalificacionService

--- src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/ICalificacionService.java
+++ src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/ICalificacionService.java
@@ -1,2 +1,10 @@
-package com.grupo04.tf_arquiweb.serviceinterfaces;public interface ICalificacionService {
+package com.grupo04.tf_arquiweb.serviceinterfaces;
+
+import com.grupo04.tf_arquiweb.entities.Calificacion;
+
+import java.util.List;
+
+public interface ICalificacionService {
+    public void insert(Calificacion calificacion);
+    public List<Calificacion> list();
 }
