89ba23a1069a038efeb8cfdfe39d0c204ce6f81a
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-09-05 23:19:43 -0500

    Se agregó Calificación DTO

--- src/main/java/com/grupo04/tf_arquiweb/dtos/CalificacionDTO.java
+++ src/main/java/com/grupo04/tf_arquiweb/dtos/CalificacionDTO.java
@@ -1,2 +1,45 @@
-package com.grupo04.tf_arquiweb.dtos;public class CalificacionDTO {
+package com.grupo04.tf_arquiweb.dtos;
+
+import com.grupo04.tf_arquiweb.entities.Reserva;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+public class CalificacionDTO {
+    private int CalificacionID;
+    private int CalificacionEstrellas;
+    private String CalificacionComentario;
+    private Reserva reserva;
+
+    public int getCalificacionID() {
+        return CalificacionID;
+    }
+
+    public void setCalificacionID(int calificacionID) {
+        CalificacionID = calificacionID;
+    }
+
+    public int getCalificacionEstrellas() {
+        return CalificacionEstrellas;
+    }
+
+    public void setCalificacionEstrellas(int calificacionEstrellas) {
+        CalificacionEstrellas = calificacionEstrellas;
+    }
+
+    public String getCalificacionComentario() {
+        return CalificacionComentario;
+    }
+
+    public void setCalificacionComentario(String calificacionComentario) {
+        CalificacionComentario = calificacionComentario;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
 }
