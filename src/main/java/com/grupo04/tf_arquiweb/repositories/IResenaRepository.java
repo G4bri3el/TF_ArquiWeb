648cb9a323ddddb1c2d2bc953c6e859d41b1896b
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-09-06 19:40:25 -0500

    Cambio a Resena en entities

--- src/main/java/com/grupo04/tf_arquiweb/repositories/IResenaRepository.java
+++ src/main/java/com/grupo04/tf_arquiweb/repositories/IResenaRepository.java
@@ -1,10 +1,10 @@
 package com.grupo04.tf_arquiweb.repositories;
 
-import com.grupo04.tf_arquiweb.entities.Calificacion;
+import com.grupo04.tf_arquiweb.entities.Reseña;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICalificacionRepository extends JpaRepository<Calificacion,Integer> {
+public interface ICalificacionRepository extends JpaRepository<Reseña,Integer> {
 
 }
