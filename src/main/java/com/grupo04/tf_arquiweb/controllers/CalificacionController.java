5b8cf694383428547bb76804d79c7483e5fbcd49
#G4br13l <gabitoal28@gmail.com>
2023-09-06 19:18:40 -0500

    Update project

--- src/main/java/com/grupo04/tf_arquiweb/controllers/CalificacionController.java
+++ src/main/java/com/grupo04/tf_arquiweb/controllers/CalificacionController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalificacionController {
     @Autowired
     private ICalificacionService cS;
+
     @PostMapping
     public void registrar(@RequestBody CalificacionDTO dto){
         ModelMapper m=new ModelMapper();
