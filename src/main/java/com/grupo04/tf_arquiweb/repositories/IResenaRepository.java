16ef206b2efee814290c04a79db523406858be13
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-09-06 19:43:33 -0500

    ResenaRepository

--- src/main/java/com/grupo04/tf_arquiweb/repositories/IResenaRepository.java
+++ src/main/java/com/grupo04/tf_arquiweb/repositories/IResenaRepository.java
@@ -1,10 +1,10 @@
 package com.grupo04.tf_arquiweb.repositories;
 
-import com.grupo04.tf_arquiweb.entities.Reseña;
+import com.grupo04.tf_arquiweb.entities.Resena;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICalificacionRepository extends JpaRepository<Reseña,Integer> {
+public interface IResenaRepository extends JpaRepository<Resena,Integer> {
 
 }
