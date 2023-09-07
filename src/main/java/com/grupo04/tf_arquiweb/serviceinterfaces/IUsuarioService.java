84019619c2d5520158bcf2ce9052aada24199f5d
#G4br13l <gabitoal28@gmail.com>
2023-09-06 19:06:31 -0500

    Update project

--- src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/IUsuarioService.java
+++ src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/IUsuarioService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface IUsuarioService { //Se pone todos los metodos que tendra la entidad
 
-    public void insert(Usuario user);
+    public void insert(Usuario usuario);
 
     public List<Usuario> list();
     public void delete(int UsuarioId);
