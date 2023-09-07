0b7f340444926620755e0332fd53b8612f636099
Mafer Medina <medina.mafer27@gmail.com>
2023-09-07 00:48:06 -0500

    Añadiendo seguridad 1

--- src/main/java/com/grupo04/tf_arquiweb/entities/Horario.java
+++
@@ -1,8 +0,0 @@
-package com.grupo04.tf_arquiweb.entities;
-
-public class Horario {
-
-    private int HorarioID;
-
-
-}
---
+++ src/main/java/com/grupo04/tf_arquiweb/security/WebSecurityConfig.java
@@ -0,0 +1,2 @@
+package com.grupo04.tf_arquiweb.security;public class WebSecurityConfig {
+}
