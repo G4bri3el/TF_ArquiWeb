daab566ebca65e4d61065d63856356b417ee43be
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-09-06 19:46:40 -0500

    cambio a resenaService

---
+++ src/main/java/com/grupo04/tf_arquiweb/serviceinterfaces/IResenaService.java
@@ -0,0 +1,11 @@
+package com.grupo04.tf_arquiweb.serviceinterfaces;
+
+import com.grupo04.tf_arquiweb.entities.Resena;
+
+import java.util.List;
+
+public interface IResenaService {
+    public void insert(Resena resena);
+    public List<Resena> list();
+    public void delete(int Resena);
+}
