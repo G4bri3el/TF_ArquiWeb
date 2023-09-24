6774ac219e4818d7592e3d8210d3afbe2cb47718
iShinmazinger <mazinger05x72@gmail.com>
2023-09-24 08:50:21 -0500

    Query en el reservarepository añadido

--- src/main/java/com/grupo04/tf_arquiweb/repositories/IReservaRepository.java
+++ src/main/java/com/grupo04/tf_arquiweb/repositories/IReservaRepository.java
@@ -2,10 +2,14 @@ package com.grupo04.tf_arquiweb.repositories;
 
 import com.grupo04.tf_arquiweb.entities.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IReservaRepository extends JpaRepository<Reserva, Integer> {
-
-
+@Query(value = "select r.ReservaId, u.UsuarioId, u.UsuarioNombre, r.ReservaFechaInicio, r.ReservaFechaFin, r.ReservaMontoTotal" +
+        " from Reserva r inner join Usuario u on r.ReservaId=u.UsuarioId Group by r.UsuarioId",nativeQuery = true)
+    List<Reserva> reservasPorEmpresario(int usuarioid);
 }
