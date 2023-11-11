a5230106705159aaec2d1c3b542673d9d33ad22d
iShinmazinger <mazinger05x72@gmail.com>
2023-11-10 20:14:54 -0500

    act 2

--- src/main/java/com/grupo04/tf_arquiweb/controllers/MediaController.java
+++ src/main/java/com/grupo04/tf_arquiweb/controllers/MediaController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -22,6 +23,7 @@ public class MediaController {
     private final HttpServletRequest request;
 
     @PostMapping("/upload")
+    @PreAuthorize("hasAuthority('EMPRESARIO') OR hasAuthority('ADMIN')")
     public Map<String, String> upload(@RequestParam("file")MultipartFile multipartFile){
         String path = storageService.store(multipartFile);
         String host = request.getRequestURL().toString().replace(request.getRequestURI(), "");
@@ -35,6 +37,7 @@ public class MediaController {
     }
 
     @GetMapping("/{filename:.+}")
+    @PreAuthorize("hasAuthority('EMPRESARIO') OR hasAuthority('ADMIN')")
     public ResponseEntity<Resource> getFile(@PathVariable String filename) throws IOException {
         Resource file = storageService.load(filename);
         String contentType = Files.probeContentType(file.getFile().toPath());
